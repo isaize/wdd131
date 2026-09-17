ce08c9db4c2e58089bff53d4134076ba4a447c9d
isaize <isaize@byupathway.edu>
2026-09-17 12:03:51 +0200

    w2 responsive design

---
+++ Week02/scripts/temples.js
@@ -0,0 +1,11 @@
+const today = new Date();
+document.getElementById("currentyear").textContent = today.getFullYear();
+document.getElementById("last-modified").textContent = document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
